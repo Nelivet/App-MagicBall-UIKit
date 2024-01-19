313a2bf40a7cd264d22925beb74e02942e189d57
Ivette Fernández <ivettefernandezrodriguez@gmail.com>
2024-01-19 16:16:55 -0500

    Initial Commit

---
+++ App-MagicBall-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-MagicBall-UIKit
+//
+//  Created by Ivette Fernandez on 19/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
